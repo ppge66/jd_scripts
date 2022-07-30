4dac64083d2327324cf58f18aa0bc2f128a4df91
ppge66 <ppge66@gmail.com>
2022-07-30 22:30:55 +0800

    改岩石

--- jd_gua_cleancart.js
+++ jd_gua_cleancart.js
@@ -224,6 +224,8 @@ async function run() {
     if (!$.out) {
       console.log('等待60秒')
       await $.wait(parseInt(Math.random() * 2000 + 60000, 10))
+      if ($.index % 5 == 0) console.log('休息1分钟，别被黑ip了\n可持续发展');
+      if ($.index % 5 == 0) await $.wait(parseInt(Math.random() * 5000 + 60000, 10));
     }
   } catch (e) {
     console.log(e)
